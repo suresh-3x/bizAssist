94701cc5cec952d4f415558dcdebf776aa5561e8
suresh-3x <77671068+suresh-3x@users.noreply.github.com>
2024-06-05 12:23:13 +0530

    updated contents

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,19 +16,16 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
+                🔥 Looking to craft a cutting-edge digital product?
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
+                Look no further than ! {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
+                Smacks 🔥
                 </span>
               </h1>
               <p>
-                Solid Pro - Packed with all the key integrations you need for
-                swift SaaS startup launch, including - Auth, Database, Sanity
-                Blog, Essential Components, Pages and More. Built-winth -
-                Next.js 13, React 18 and TypeScript.
+                Smacks is your one-stop shop for bringing your digital vision to life. We're a team of passionate experts dedicated to helping businesses like yours build impactful websites, apps, and other technical solutions.
               </p>
 
               <div className="mt-10">
--- components/Pricing/index.tsx
+++ components/Pricing/index.tsx
@@ -14,14 +14,14 @@ const Pricing = () => {
               headerInfo={{
                 title: `PRICING PLANS`,
                 subtitle: `Simple Pricing`,
-                description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam ante in maximus.`,
+                description: `The pricing will be as per the project. We usually charge based on the number of hours required for a project, but we have the option to price our services based on the project so that we can deliver the project early with less dev hours.`,
               }}
             />
           </div>
           {/* <!-- Section Title End --> */}
         </div>
 
-        <div className="relative mx-auto mt-15 max-w-[1207px] px-4 md:px-8 xl:mt-20 xl:px-0">
+        {/* <div className="relative mx-auto mt-15 max-w-[1207px] px-4 md:px-8 xl:mt-20 xl:px-0">
           <div className="absolute -bottom-15 -z-1 h-full w-full">
             <Image
               fill
@@ -30,9 +30,9 @@ const Pricing = () => {
               className="dark:hidden"
             />
           </div>
-          <div className="flex flex-wrap justify-center gap-7.5 lg:flex-nowrap xl:gap-12.5">
+          <div className="flex flex-wrap justify-center gap-7.5 lg:flex-nowrap xl:gap-12.5"> */}
             {/* <!-- Pricing Item --> */}
-            <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-strokedark dark:bg-blacksection dark:shadow-none md:w-[45%] lg:w-1/3 xl:p-12.5">
+            {/* <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-strokedark dark:bg-blacksection dark:shadow-none md:w-[45%] lg:w-1/3 xl:p-12.5">
               <h3 className="mb-7.5 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle3">
                 $10{" "}
                 <span className="text-regular text-waterloo dark:text-manatee">
@@ -80,10 +80,10 @@ const Pricing = () => {
                   />
                 </svg>
               </button>
-            </div>
+            </div> */}
 
             {/* <!-- Pricing Item --> */}
-            <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-strokedark dark:bg-blacksection dark:shadow-none md:w-[45%] lg:w-1/3 xl:p-12.5">
+            {/* <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-strokedark dark:bg-blacksection dark:shadow-none md:w-[45%] lg:w-1/3 xl:p-12.5">
               <div className="absolute -right-3.5 top-7.5 -rotate-90 rounded-bl-full rounded-tl-full bg-primary px-4.5 py-1.5 text-metatitle font-medium uppercase text-white">
                 popular
               </div>
@@ -135,10 +135,10 @@ const Pricing = () => {
                   />
                 </svg>
               </button>
-            </div>
+            </div> */}
 
             {/* <!-- Pricing Item --> */}
-            <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-strokedark dark:bg-blacksection dark:shadow-none md:w-[45%] lg:w-1/3 xl:p-12.5">
+            {/* <div className="animate_top group relative rounded-lg border border-stroke bg-white p-7.5 shadow-solid-10 dark:border-strokedark dark:bg-blacksection dark:shadow-none md:w-[45%] lg:w-1/3 xl:p-12.5">
               <h3 className="mb-7.5 text-3xl font-bold text-black dark:text-white xl:text-sectiontitle3">
                 $189{" "}
                 <span className="text-regular text-waterloo dark:text-manatee">
@@ -186,9 +186,9 @@ const Pricing = () => {
                   />
                 </svg>
               </button>
-            </div>
-          </div>
-        </div>
+            </div> */}
+          {/* </div> */}
+        {/* </div> */}
       </section>
       {/* <!-- ===== Pricing Table End ===== --> */}
     </>
