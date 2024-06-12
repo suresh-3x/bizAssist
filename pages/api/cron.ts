b33c6a55548efa3a231bb24907e85e656f249262
suresh-3x <77671068+suresh-3x@users.noreply.github.com>
2024-06-13 01:48:39 +0530

    fixed cron job

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -6,6 +6,9 @@ export default async function handler(req, res) {
   if (!uri) {
     return res.status(500).json({ error: 'MongoDB URI is not defined' });
   }
+  
+  const { startDate, endDate } = req.query;
+
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -17,66 +20,80 @@ export default async function handler(req, res) {
     const database = client.db('Cluster0');
     const collection = database.collection('emails');
 
+    // Calculate start of today in IST
     const currentDate = new Date();
-    const startOfDayIST = new Date(currentDate.toLocaleString('en-US', {timeZone: 'Asia/Kolkata'}));
-    startOfDayIST.setHours(0, 0, 0, 0);
 
-    const startOfDayUTC = new Date(startOfDayIST).toISOString();
-    const currentDateUTC = new Date().toISOString();
+    let startOfDayUTC;
+    if (startDate) {
+      const startDateTime = new Date(startDate);
+      startOfDayUTC = startDateTime.toISOString();
+    } else {
+      const startOfDayIST = new Date(currentDate.toLocaleString('en-US', {timeZone: 'Asia/Kolkata'}));
+      startOfDayIST.setHours(0, 0, 0, 0);
+      startOfDayUTC = new Date(startOfDayIST).toISOString();
+    }
+
+    let currentDateUTC;
+    if (endDate) {
+      const endDateTime = new Date(endDate);
+      currentDateUTC = endDateTime.toISOString();
+    } else {
+      currentDateUTC = currentDate.toISOString();
+    }
 
     const emails = await collection.find({
-        timestamp: { $gte: startOfDayUTC, $lte: currentDateUTC }
-      }).toArray();
+      timestamp: { $gte: startOfDayUTC, $lte: currentDateUTC }
+    }).toArray();
 
-      emails.forEach(email => {
+    emails.forEach(email => {
       email.timestamp = new Date(email.timestamp).toLocaleString('en-IN', {timeZone: 'Asia/Kolkata'});
     });
 
     let tableHtml = `
-    <style>
-      table {
-        width: 100%;
-        border-collapse: collapse;
-        font-family: Arial, sans-serif;
-      }
-      th, td {
-        padding: 8px;
-        text-align: left;
-        border-bottom: 1px solid #ddd;
-      }
-      th {
-        background-color: #f2f2f2;
-      }
-      tbody tr:nth-child(even) {
-        background-color: #f2f2f2;
-      }
-      tbody tr:hover {
-        background-color: #ddd;
-      }
-    </style>
-    <table>
-      <thead>
-        <tr>
-          <th>Email</th>
-          <th>Timestamp (IST)</th>
-        </tr>
-      </thead>
-      <tbody>
-  `;
-  
-  emails.forEach(email => {
-    tableHtml += `
-      <tr>
-        <td>${email.email}</td>
-        <td>${email.timestamp}</td>
-      </tr>
+      <style>
+        table {
+          width: 100%;
+          border-collapse: collapse;
+          font-family: Arial, sans-serif;
+        }
+        th, td {
+          padding: 8px;
+          text-align: left;
+          border-bottom: 1px solid #ddd;
+        }
+        th {
+          background-color: #f2f2f2;
+        }
+        tbody tr:nth-child(even) {
+          background-color: #f2f2f2;
+        }
+        tbody tr:hover {
+          background-color: #ddd;
+        }
+      </style>
+      <table>
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Timestamp (IST)</th>
+          </tr>
+        </thead>
+        <tbody>
     `;
-  });
-  
-  tableHtml += '</tbody></table>';
+    
+    emails.forEach(email => {
+      tableHtml += `
+        <tr>
+          <td>${email.email}</td>
+          <td>${email.timestamp}</td>
+        </tr>
+      `;
+    });
+    
+    tableHtml += '</tbody></table>';
 
-    const startDateIST = startOfDayIST.toLocaleString('en-IN', {timeZone: 'Asia/Kolkata'});
-    const endDateIST = currentDate.toLocaleString('en-IN', {timeZone: 'Asia/Kolkata'});
+    const startDateIST = new Date(startOfDayUTC).toLocaleString('en-IN', {timeZone: 'Asia/Kolkata'});
+    const endDateIST = new Date(currentDateUTC).toLocaleString('en-IN', {timeZone: 'Asia/Kolkata'});
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -86,7 +103,6 @@ export default async function handler(req, res) {
       }
     });
 
-    
     const mailOptions = {
       from: process.env.GMAIL_SMTP_EMAIL_FROM,
       to: 'info@bizassist.online',
